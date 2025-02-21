d8ff11bd8f2f800ea5aeb62c381a22c3879bf052
tiennguyenvan <nguyentien.jobs@gmail.com>
2025-02-20 22:01:09 -0800

    Switched btw fragment succcessfully

--- code/app/src/main/java/com/example/mealclue/view/activities/MainActivity.java
+++ code/app/src/main/java/com/example/mealclue/view/activities/MainActivity.java
@@ -8,16 +8,20 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.mealclue.R;
-
+import com.example.mealclue.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding $;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        $ = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView($.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        NavController navController = Navigation.findNavController(this, R.id.frgMain);
+        NavigationUI.setupWithNavController($.navMainMenu, navController);
+    }
+
 }
